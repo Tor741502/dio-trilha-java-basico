0932c68eca6032c9ab937a9cfee35e762779f28d
Giovanni <bee.t.lombardi@gmail.com>
2023-04-22 19:12:38 -0300

    d

---
+++ controle-de-fluxo/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    String escrever = " ";
+
+    public void escrever(String muda){
+        escrever = muda;
+        System.out.println(escrever);
+    }
+    
+    
+}
---
+++ controle-de-fluxo/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) throws Exception{
+        App app = new App();
+
+        app.escrever("Controle do fluxo serve para que eu possa alterar as ordens das paradas");
+        app.escrever("Existem alguns tipos como os de decisão, repetição e interrupção");
+        
+        app.escrever("Os de decisão são os if os else os swicht e etc");
+        app.escrever("Estes sao os que avaliam uma extrutura booleana ou variavel para controlar e mudar  o fluxo");
+
+        app.escrever("Os de repetição são os for ou while");
+
+        app.escrever("E os de interrupção são os break, continue(sim é ironico) e return");
+    }
+}
