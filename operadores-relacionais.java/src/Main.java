244d87b061a2d1714464c58e76aaedb8f19ff17f
Giovanni <bee.t.lombardi@gmail.com>
2023-04-22 08:10:59 -0300

    sla

---
+++ operadores-relacionais.java/src/Main.java
@@ -0,0 +1,52 @@
+
+
+public class Main{
+    public static void main(String[] args){
+        int i1, i2;
+        float f1, f2;
+        double d1 = 59.6;
+        char c1, c2;
+        String s1, s2, s3;
+        boolean b1, b2;
+        long l1, l2;
+        byte y1 = 1;
+        short h1 = 25;
+
+        l1 = 1597l;
+        l2 = 8997l;
+
+        b1 = true;
+        b2 = false;
+
+
+        s1 = "Fulano";
+        s2 = "Beltrano";
+        s3 = " siclano";
+
+
+        c1 = 'x';
+        c2 = 'y';
+
+        i1 = 10;
+        i2 = 20;
+
+        f1 = 4.5f;
+        f2 = 3.5f;
+
+        System.out.println("i1 == i2 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+        System.out.println("i1 > i2 " + (i1 > i2));
+        System.out.println("i1 <= i2 " + (i1 <= i2));
+
+
+
+
+        
+        
+
+
+        
+
+        
+    }
+}
