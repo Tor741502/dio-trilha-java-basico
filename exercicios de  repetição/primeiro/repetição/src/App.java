b4aa9605492e65e4ff6b2a950c168523f160f5dc
Giovanni <bee.t.lombardi@gmail.com>
2023-04-23 19:49:36 -0300

    sda

---
+++ exercicios de  repetição/primeiro/repetição/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        System.out.println("informe o seu nome: ");
+        nome = input.next();
+
+        System.out.println("informe sua idade: ");
+        idade = input.nextInt();
+
+
+        
+    }
+}
