f294cf288ff57556c5427f83976ada5d93bc8838
Giovanni <bee.t.lombardi@gmail.com>
2023-04-23 18:26:40 -0300

    s

--- quimicas/src/App.java
+++ quimicas/src/App.java
@@ -13,19 +13,13 @@ public class App {
         n = scanner.nextInt();
         
         aM = aN + n;
-        String elementSymbol = getElementSymbol(aN);
         
         System.out.println("(Z) Número atômico: " + aN);
         System.out.println("(A) Número de massa: " + aM);
-        System.out.println("Elemento químico: " + elementSymbol);
 
         scanner.close();
     }
     
-    public static String getElementSymbol(int atomicNumber) {
-        String[] elementSymbols = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"};
-        
-        return elementSymbols[atomicNumber - 1];
-    }
+
 }
 
