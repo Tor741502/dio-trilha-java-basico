789edac3a343bec1cec5f3ae080cc35ee00144e1
Tor741502 <bee.t.lombardi@gmail.com>
2023-04-28 13:05:04 -0300

    Agora com o Scanner junto =)

--- orientacaodeobjetos/src/rodarPrograma.java
+++ orientacaodeobjetos/src/rodarPrograma.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class rodarPrograma {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Carro caro1 = new Carro();
 
-        caro1.setColor("Azul");
-        caro1.setModel("Um modelo de carro");
-        caro1.setTankCapacity(59);
+        System.out.println("Digite a cor do carro: ");
+        caro1.setColor(input.nextLine());
+        System.out.println("Digite o modelo do carro: ");
+        caro1.setModel(input.nextLine());
+        System.out.println("Digite a capacidade do tanque: ");
+        caro1.setTankCapacity(input.nextInt());
 
-        System.out.println(caro1.getColor());
-        System.out.println(caro1.getModel());
-        System.out.println(caro1.getTankCapacity());
-        System.out.println(caro1.totValueTank(6.39));
+        System.out.println("A cor  do seu carro é " + caro1.getColor());
+        System.out.println("O modelo do seu carro é " + caro1.getModel());
+        System.out.println(" A capacidade do seu tanque é " + caro1.getTankCapacity());
+        System.out.println("O valor total do taanque é " + caro1.totValueTank(6.46));
 
         Carro carro2 = new Carro("cinza", "mercedes", 66);
 
