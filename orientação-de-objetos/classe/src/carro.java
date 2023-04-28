bb875ce754f69eba33731169f78c85d4874b9bf7
Tor741502 <bee.t.lombardi@gmail.com>
2023-04-28 11:54:56 -0300

    agora no intellij =)

--- orientação-de-objetos/classe/src/carro.java
+++ orientação-de-objetos/classe/src/carro.java
@@ -1,7 +1,6 @@
 
-
-class carro{
-    String cor;
-    String modelo;
-    int capacidadeTanque;
+public class carro {
+    public static void main(String[] args) {
+        System.out.println("Hello master!");
+    }
 }
